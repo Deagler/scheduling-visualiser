e9abf9d06ca224ddb62298f5977799b3d9988658
william-shin-387 <38583073+william-shin-387@users.noreply.github.com>
2019-08-03 18:57:47 +1200

    Create Task,Graph,Scheduler,Allocation classes

---
+++ src/main/java/internseason/scheduler/model/Graph.java
@@ -0,0 +1,14 @@
+package internseason.scheduler.model;
+
+import java.util.List;
+
+public class Graph {
+    List<Task> tasks;
+
+    public Graph() {
+    }
+
+    public Graph(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+}
---
+++ src/main/java/internseason/scheduler/model/Task.java
@@ -0,0 +1,17 @@
+package internseason.scheduler.model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Task {
+        Map<Task, Integer> children;
+        int cost;
+        String id;
+
+        public Task(int cost, String id) {
+            this.cost = cost;
+            this.id = id;
+            children = new HashMap<>();
+        }
+}
+
