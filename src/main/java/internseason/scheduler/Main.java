c1b26fff2b2fb5ace98d79a91829ebf8b890f101
Sukhans Asrani <sukhansnz@gmail.com>
2019-08-03 15:45:05 +1200

    Fix gradle.build and add Main class

---
+++ src/main/java/internseason/scheduler/Main.java
@@ -0,0 +1,39 @@
+package internseason.scheduler;
+
+import org.apache.commons.cli.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Options options = new Options();
+
+        Option input = new Option("i", "input", true, "input file path");
+        input.setRequired(true);
+        options.addOption(input);
+
+        Option output = new Option("o", "output", true, "output file");
+        output.setRequired(true);
+        options.addOption(output);
+
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+        CommandLine cmd = null;
+
+        try {
+            cmd = parser.parse(options, args);
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+            formatter.printHelp("utility-name", options);
+
+            System.exit(1);
+        }
+
+        String inputFilePath = cmd.getOptionValue("input");
+        String outputFilePath = cmd.getOptionValue("output");
+
+        System.out.println(inputFilePath);
+        System.out.println(outputFilePath);
+    }
+
+}
---
+++ src/main/java/internseason/scheduler/visual/FXMain.java
@@ -0,0 +1,13 @@
+package internseason.scheduler.visual;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class FXMain extends Application {
+
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
