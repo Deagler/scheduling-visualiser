9686f42db4ee43efa1ea056ca4ec90b6b63656f8
CWON880-SURFACE\cwong <cwong367@gmail.com>
2019-08-04 21:30:34 +1200

    Created Edge class abstracting the dependencies on a DAG

--- src/main/java/internseason/scheduler/model/Task.java
+++ src/main/java/internseason/scheduler/model/Task.java
@@ -1,17 +1,23 @@
 package internseason.scheduler.model;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Task {
-        Map<Task, Integer> children;
-        int cost;
-        String id;
+
+
+        private List<Edge> incomingEdge;
+        private List<Edge> outgoingEdge;
+        private int cost;
+        private String id;
 
         public Task(int cost, String id) {
             this.cost = cost;
             this.id = id;
-            children = new HashMap<>();
+            incomingEdge = new ArrayList<>();
+            outgoingEdge = new ArrayList<>();
         }
 
         public int getCost() {
