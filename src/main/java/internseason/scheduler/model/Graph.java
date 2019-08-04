38dbccaf9035e89266319c0fb78ed3c90bc1454f
Andrew Hu <andrew.h318@hotmail.co.nz>
2019-08-04 23:59:58 +1200

    move adj list to graph calss:

--- src/main/java/internseason/scheduler/model/Graph.java
+++ src/main/java/internseason/scheduler/model/Graph.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class Graph {
     Map<String, Task> tasks;
     List<Dependency> dependencies;
+    Map<String, List<String>> adjacencyList;
     List<List<String>> topologicalOrdering;
 
     public Graph() {
@@ -38,20 +39,35 @@ public class Graph {
     private void createTopologicalOrdering() {
         //need adjacency list representation of the graph
         /*1. Store each vertex’s In-Degree in an array
-2. Initialize a queue with all in-degree zero vertices
-3. While there are vertices remaining in the queue:
-➭ Dequeue and output a vertex
-➭ Reduce In-Degree of all vertices adjacent to it by 1
-➭ Enqueue any of these vertices whose In-Degree became
-zero*/
+        2. Initialize a queue with all in-degree zero vertices
+        3. While there are vertices remaining in the queue:
+        ➭ Dequeue and output a vertex
+        ➭ Reduce In-Degree of all vertices adjacent to it by 1
+        ➭ Enqueue any of these vertices whose In-Degree became
+        zero*/
 
         int[] inDegrees = new int[tasks.size()];
         //inDegrees = getInDegrees();
+    }
 
+    private Map<String, List<String>> getAdjacencyList() {
+        if (this.adjacencyList != null) {
+            return this.adjacencyList;
+        }
 
+        this.adjacencyList = new HashMap<>();
 
+        for(Dependency dependency : this.dependencies) {
+            String sourceId = dependency.getSourceTask().getId();
+            String targetId = dependency.getTargetTask().getId();
 
+            List<String> list = adjacencyList.containsKey(sourceId) ? adjacencyList.get(sourceId) : new ArrayList<>();
+            list.add(targetId);
 
+            adjacencyList.put(sourceId, list);
+        }
+
+        return adjacencyList;
     }
 
     private void getInDegrees() {
