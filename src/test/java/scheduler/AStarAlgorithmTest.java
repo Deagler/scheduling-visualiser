58afe1a4632355b6bfb938d7c2557c90f58b81df
William <willyy387@gmail.com>
2019-08-09 15:22:09 +1200

    added assertEquals for testing AStarSchedule()

--- src/test/java/scheduler/AStarAlgorithmTest.java
+++ src/test/java/scheduler/AStarAlgorithmTest.java
@@ -10,6 +10,8 @@ import org.junit.Test;
 import java.util.HashMap;
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
+
 public class AStarAlgorithmTest {
     private Graph graph;
     private DOTParser parser;
@@ -24,7 +26,16 @@ public class AStarAlgorithmTest {
             Graph graph = this.parser.parse("src/test/resources/Test_Diamond.dot");
             AStarAlgorithm algorithm = new AStarAlgorithm(graph,2);
             Schedule schedule = algorithm.execute();
-            System.out.println(schedule);
+            //System.out.println(schedule);
+            assertEquals("Processor 0\n" +
+                    "t1 scheduled at: 4\n" +
+                    "t3 scheduled at: 6\n" +
+                    "Cost of Processor 0: 8\n" +
+                    "Processor 1\n" +
+                    "t0 scheduled at: 0\n" +
+                    "t2 scheduled at: 2\n" +
+                    "Cost of Processor 1: 5\n" +
+                    "Total schedule cost is: 8", schedule.toString());
         } catch (InputException e) {
             e.printStackTrace();
         }
