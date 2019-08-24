54e9f8c8cada9e5927584566314e93a393d15541
Casey Wong <cwong367@gmail.com>
2019-08-24 15:50:22 +1200

    Change how we build top order so it accounts for alphabet and nodes not starting at 0

--- src/main/java/internseason/scheduler/model/Graph.java
+++ src/main/java/internseason/scheduler/model/Graph.java
@@ -48,6 +48,10 @@ public class Graph {
         }
         this.adjacencyList = new HashMap<>();
 
+        for (Task task : tasks.values()){
+            adjacencyList.put(task.getId(), new ArrayList<>());
+        }
+
         for(Dependency dependency : this.dependencies) {
             String sourceId = dependency.getSourceTask().getId();
             String targetId = dependency.getTargetTask().getId();
@@ -63,56 +67,82 @@ public class Graph {
     private void createTopologicalOrdering() {
         // Store the in degree of array
         adjacencyList = getAdjacencyList();
-        int[] inDegrees = getInDegrees(adjacencyList);
 
-        Set<Integer> visitedSet = new HashSet<>();
+        Map<String, Integer> inDegrees = buildInDegrees(adjacencyList);
+        inDegrees = getInDegrees(adjacencyList, inDegrees);
+        Set<String> visitedSet = new HashSet<>();
         topologicalOrdering = new ArrayList<>();
 
         // Initialize a queue with all in-degree zero vertices
-        LinkedList<Integer> zeroDegrees = new LinkedList<>();
-        for (int i=0; i<inDegrees.length;i++){
-            if (inDegrees[i] == 0){
-                zeroDegrees.add(i);
+        LinkedList<String> zeroDegrees = new LinkedList<>();
+        for (String task : inDegrees.keySet()){
+            if (inDegrees.get(task) == 0){
+                zeroDegrees.add(task);
             }
         }
-
         List<List<String>> layers = new ArrayList<>();
         //while there are vertices remaining in the queue
         while (!zeroDegrees.isEmpty()){
-
-
             visitedSet.addAll(zeroDegrees);
-
             List<String> layer  = new ArrayList<>();
-            for (Integer vertex : zeroDegrees) {
+            for (String vertex : zeroDegrees) {
                 adjacencyList.remove(String.valueOf(vertex));
                 layer.add(String.valueOf(vertex));
             }
             layers.add(layer);
-
             zeroDegrees.clear();
-
-
             //enqueue any vertice whose in degree became zero
-            inDegrees = getInDegrees(adjacencyList);
-            for (int i=0;i<inDegrees.length;i++){
-                if (inDegrees[i] == 0 && !visitedSet.contains(i) && !zeroDegrees.contains(i)){
-                    zeroDegrees.add(i);
+            inDegrees = getInDegrees(adjacencyList, inDegrees);
+            for (String task : inDegrees.keySet()){
+                if (inDegrees.get(task) == 0 && !visitedSet.contains(task) && !zeroDegrees.contains(task)) {
+                    zeroDegrees.add(task);
                 }
             }
         }
-
         // convert to task objects
         for (List<String> layer : layers) {
             topologicalOrdering.add(buildTaskListFromIds(layer));
         }
     }
 
-    private int[] getInDegrees(Map<String,List<String>> adj) {
-        int[] inDegrees = new int[tasks.size()];
+    /** Build the initial map of task ids to their indegree count
+     * @param adj adjacency list of graph
+     * @return map where indegree of each node is zero
+     */
+    private Map<String,Integer> buildInDegrees(Map<String, List<String>> adj){
+        HashMap<String, Integer> inDegrees = new HashMap<>();
+        for (String task : adj.keySet()){
+            inDegrees.put(task,0);
+        }
+        for (List<String> tasks : adj.values()){
+            for (String task : tasks){
+                if (!inDegrees.containsKey(task)){
+                    inDegrees.put(task,0);
+                }
+            }
+        }
+        return inDegrees;
+    }
+
+    /** Loop through adjaceny list and increment indegree value of each task in the indegree map
+     * @param adj adjaceny list of graph
+     * @param inDegrees map of tasks to their current indegree
+     * @return
+     */
+    private Map<String,Integer> getInDegrees(Map<String,List<String>> adj, Map<String, Integer> inDegrees) {
+
+        for (String task : inDegrees.keySet()){
+            inDegrees.put(task,0);
+        }
         for (List<String> value : adj.values()){
             for (String s : value){
-                inDegrees[Integer.parseInt(s)]++;
+                if (inDegrees.containsKey(s)){
+                    int count = inDegrees.get(s);
+                    count++;
+                    inDegrees.put(s, count);
+                } else {
+                    inDegrees.put(s,1);
+                }
             }
         }
         return inDegrees;
